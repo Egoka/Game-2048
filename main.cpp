fbba8c1b97fdce316cb5b7b21fdf0197281ff04e
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-07-30 23:40:02 +0300

    Window display loop
    An open window condition loop is created.
    Internally, the functions of cleaning and displaying the application window are launched.

--- main.cpp
+++ main.cpp
@@ -5,5 +5,11 @@ using namespace sf;
 int main()
 {
     RenderWindow window(VideoMode(870, 870), "2048");
+    window.setKeyRepeatEnabled(false);
+    while (window.isOpen())
+    {
+        window.clear();
+        window.display();
+    }
     return 0;
 }

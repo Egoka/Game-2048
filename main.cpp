19f4e344bd799cd928291a486a824b3f2a0bfd28
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-07-30 23:43:43 +0300

    Display background
    A variable from the Texture class is created. after which the background image rash.jpg is written into it.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,13 @@ int main()
 {
     RenderWindow window(VideoMode(870, 870), "2048");
     window.setKeyRepeatEnabled(false);
+    Texture pole;
+    pole.loadFromFile("img/rash.jpg");
+    Sprite sprite(pole);
     while (window.isOpen())
     {
         window.clear();
+        window.draw(sprite);
         window.display();
     }
     return 0;

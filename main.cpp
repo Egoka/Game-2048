054df2cdc0eef01dad8d929297b560ee6299c383
Egorkassss <44812777+Egorkassss@users.noreply.github.com>
2020-08-08 00:16:58 +0300

    Formation of the event matrix
    A 4 by 4 matrix is formed. To track the state of numbers.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <SFML/Graphics.hpp>
 using namespace std;
 using namespace sf;
+int arr[4][4];
 int main()
 {
     RenderWindow window(VideoMode(870, 870), "2048");
@@ -9,6 +10,9 @@ int main()
     Texture pole;
     pole.loadFromFile("img/rash.jpg");
     Sprite sprite(pole);
+    for (auto& i : arr)
+        for (int& j : i)
+            j = 0;
     while (window.isOpen())
     {
         window.clear();
